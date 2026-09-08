81c85d930f7dff16a974828c0809fee480ba7780
VS Code <you@example.com>
2026-09-08 21:58:27 +1000

    Initial commit from VS Code

---
+++ Card/src/Payment.java
@@ -0,0 +1,19 @@
+public class Payment {
+    public double amount;
+    public String currency;
+    public String status;
+
+    //constructor
+
+    public Payment(double amount, String currency, String status){
+        this.amount = amount;
+        this.currency = currency;
+        this.status = status;
+    }
+
+    //getters & setters
+
+     public void processpayment(){
+
+     }
+}
